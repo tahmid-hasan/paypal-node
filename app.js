d04f3f9122c6564b534b9e22e6bb095bc6e8136b
Tahmid Hasan <web.tahmid@outlook.com>
2018-06-13 18:04:18 +0600

    This the first initial commit for this project

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const ejs = require('ejs');
+const paypal = require('paypal-rest-sdk');
+const create_payment_json = require('./data/data.json');
+
+paypal.configure({
+    'mode': 'sandbox', // sandbox or live
+    'client_id': 'AU9PqWmfIRnwaK_CvahitJBChmcOMAKrM7XAsWxnzLP3g7tHdqtJ5ZDSXr0MBhwqFwO7aVniGGilZD9s',
+    'client_secret': 'EHJevcMnv9oDaxTEP_gjkzLie3U99AM-SCZBeoeFvlju_a0ThltFRBm_zpWLSfUW1hl1gxiCRsjyKVSw'
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/pay', (req, res) => {
+    paypal.payment.create(create_payment_json, (err, payment) => {
+        if(err) {
+            throw err
+        } else {
+            for(let i = 0; i < payment.links.length; i++) {
+                if(payment.links[i].rel === 'approval_url') {
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+});
+
+app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+    console.log(payerId);
+
+    const execute_payment_json = {
+        'payer_id': payerId,
+        'transactions': [{
+            'amount': {
+                'currency': 'USD',
+                'total': '28.48'
+            }
+        }]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, (err, payment) => {
+        if(err) {
+            console.log(err.response);
+            throw err;
+        } else {
+            res.json(payment);
+        }
+    });
+});
+
+app.get('/cancle', (req, res) => res.send('Cancled'))
+
+app.listen(3000, () => console.log('Server started and running on port 3000'));
